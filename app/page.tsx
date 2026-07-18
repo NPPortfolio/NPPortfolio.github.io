18b076b8bdc9338d1570f2be0ac61ff3577b7c92
JOHNSON1MILLION <11758845+NPPortfolio@users.noreply.github.com>
2026-07-18 18:40:12 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,107 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import Image from "next/image";
+import {useState} from 'react'
+
+export default function Root() {
+
+  const [currentTab, setCurrentTab] = useState(0)
+
+  // see note about context, this feels wrong having to repeat the function here
+  const [tabs, setTabs] = useState<Tab[]>([
+    {id : 0, name : "FRONT END", color : 'red', setCurrentTab : setCurrentTab},
+    {id : 1, name : "SOMETHING", color : 'purple', setCurrentTab : setCurrentTab},
+    // TODO: figure out how to make this tab a special case and have not just a solid color for a tab, or maybe each tab gets their own style
+    {id : 2, name : "PRIMITIVE_ARPG", color : 'darkGreen', setCurrentTab : setCurrentTab},
+  ])
+
+  
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div
+    style = {{
+      display : 'flex',
+      flexDirection : 'column',
+      backgroundColor : 'navy',
+      alignContent : 'center',
+      height : '100vh',
+    }}>
+      <div
+      style = {{
+        textAlign : 'center',
+        backgroundColor : 'black',
+      }}>
+        Some text thing here
+
+      </div>
+      <TabBar tabs = {tabs} setCurrentTab = {setCurrentTab} />
+      <ProjectOverview/>
     </div>
-  );
+  )
+}
+
+interface Tab {
+  id : number,
+  name : string,
+  color : string,
+  // TODO: look into context more, can have global things and no need to repeat this every tab?
+  setCurrentTab : React.Dispatch<React.SetStateAction<number>>
+}
+
+// container that has description and images maybe
+function ProjectOverview() {
+
+  return (
+    <div
+    style = {{
+      backgroundColor : 'darkorange',
+      flexGrow : '1',
+    }}>
+    </div>
+  )
+}
+
+interface TabBarProps {
+  tabs : Tab[],
+  setCurrentTab : React.Dispatch<React.SetStateAction<number>>
+}
+function TabBar({tabs, setCurrentTab} : TabBarProps) {
+
+  return (
+    <div
+    style = {{
+      display : 'flex'
+    }}>
+      {
+        tabs.map((tab) => (
+          <Tab
+            key = {tab.id}
+            id = {tab.id}
+            name = {tab.name}
+            color = {tab.color}
+            setCurrentTab={setCurrentTab}
+          />
+        ))
+      }
+    </div>
+  )
+}
+
+function Tab({id, name, color, setCurrentTab} : Tab) {
+
+  const handleMouseDown = () => {
+    setCurrentTab(id)
+  }
+
+  return (
+    <div
+      style = {{
+        backgroundColor : color,
+        flexGrow : '1',
+        textAlign : 'center',
+      }}
+      onMouseDown={handleMouseDown}
+    >
+      {name}
+    </div>
+  )
 }
